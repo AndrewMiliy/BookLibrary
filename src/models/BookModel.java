17e478654167dcd31aa2a3145523289c1b05ed13
ayselsahin <ayselrsahin@gmail.com>
2023-10-31 18:45:54 +0100

    Aysel1

--- src/models/BookModel.java
+++ src/models/BookModel.java
@@ -1,4 +1,60 @@
 package models;
 
 public class BookModel {
+    private UserRole userRole;
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    public void setUserRole(UserRole userRole) {
+        this.userRole = userRole;
+    }
+
+    public boolean isAuthorized() {
+        return userRole == UserRole.ListBook || userRole == UserRole.UserRole;
+    }
+
+    public void performActionBasedOnUserRole() {
+        switch (userRole) {
+            case ListBook:
+                System.out.println("Perform action for ListBook role.");
+                break;
+            case UserRole:
+                System.out.println("Perform action for UserRole role.");
+                break;
+            case ID:
+                System.out.println("Perform action for ID role.");
+                break;
+            case FirstName:
+                System.out.println("Perform action for FirstName role.");
+                break;
+            case LastName:
+                System.out.println("Perform action for LastName role.");
+                break;
+            case Email:
+                System.out.println("Perform action for Email role.");
+                break;
+            case HashPassword:
+                System.out.println("Perform action for HashPassword role.");
+                break;
+            default:
+                System.out.println("Unauthorized role. Cannot perform the action.");
+                break;
+        }
+    }
+    public static void main(String[] args) {
+        BookModel bookModel = new BookModel();
+        bookModel.setUserRole(UserRole.ListBook);
+
+        System.out.println("User's role: " + bookModel.getUserRole());
+
+        if (bookModel.isAuthorized()) {
+            System.out.println("User is authorized to perform this action.");
+        } else {
+            System.out.println("User is not authorized to perform this action.");
+        }
+
+        bookModel.performActionBasedOnUserRole();
+    }
 }
---
+++ src/models/UserRole.java
@@ -0,0 +1,11 @@
+package models;
+
+public enum UserRole {
+    ID,
+    FirstName,
+    LastName,
+    Email,
+    HashPassword,
+    ListBook,
+    UserRole
+}
