3cf1ef66be04f758f749b59e8345ae8ca9baa096
Kirrrra <kirrrra007@gmail.com>
2023-11-02 12:50:41 +0100

    Тесты add, getuser, getusers, remove (не полный)

--- src/test/TestBookRepository.java
+++ src/test/TestBookRepository.java
@@ -5,16 +5,9 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repositories.BookRepository;
-import repositories.ElasticArray;
-import repositories.Validate;
 
-import java.time.Instant;
 import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.util.Date;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TestBookRepository {
 
@@ -49,5 +42,59 @@ public class TestBookRepository {
         Assertions.assertFalse(books.add(testInvalidBookAdd2));
         Assertions.assertFalse(books.add(testInvalidBookAdd3));
     }
+
+    @Test
+    void testValidGetBook() {
+        BookModel testValidGetBook = new BookModel("Master i Margarita", "M. Bulgakov", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+        books.add(testValidGetBook);
+
+        BookModel retrievedBook = books.getBook(x -> x.getName().equals("Master i Margarita"));
+        Assertions.assertNotNull(retrievedBook);
+    }
+    @Test
+    void testInvalidGetBook() {
+        BookModel testInvalidGetBook = new BookModel("Master i Margarita", "M. Bulgakov", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+        books.add(testInvalidGetBook);
+
+        BookModel retrievedBook = books.getBook(x -> x.getName().equals("Master Margarita"));
+        Assertions.assertNull(retrievedBook);
+    }
+
+    @Test
+    void testValidGetBooks() {
+        BookModel testValidGetBooks = new BookModel("Master i Margarita", "M. Bulgakov", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+        books.add(testValidGetBooks);
+        Assertions.assertTrue(books.getBooks(x -> x.getName().equals("Master i Margarita")).contains(testValidGetBooks));
+    }
+
+
+    @Test
+    void testInvalidGetBooks() {
+        BookModel testInvalidGetBooks = new BookModel("Master i Margarita", "M. Bulgakov", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+        BookModel testInvalidGetBooks2 = new BookModel("Master i Margarita2", "M. Bulgakov2", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+        books.add(testInvalidGetBooks);
+        books.add(testInvalidGetBooks2);
+        Assertions.assertFalse(books.getBooks(x -> x.getName().equals("Master Margarita")).contains(testInvalidGetBooks));
+    }
+
+    @Test
+    void testValidBookRemove() {
+        BookModel testValidBookRemove = new BookModel("Master i Margarita", "M. Bulgakov", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+        books.add(testValidBookRemove);
+
+        books.remove(testValidBookRemove);
+        BookModel removedBook = books.getBook(x -> x.getName().equals("Master i Margarita"));
+        Assertions.assertNull(removedBook);
+    }
+//    @Test
+//    void testInvalidBookRemove() {
+//        BookModel testInvalidBookRemove = new BookModel("Master i Margarita", "M. Bulgakov", LocalDate.of(1960, 1, 1), "Жили были не тужили");
+//        books.add(testInvalidBookRemove);
+//
+//        books.remove(testInvalidBookRemove);
+//        BookModel removedBook = books.getBook(x -> x.getName().equals("Master i Margarita"));
+//        Assertions.assertNull(removedBook);
+//    }
+//    
 
 }
